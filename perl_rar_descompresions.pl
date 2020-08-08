d4335b0fe657b4490debf42c17875a856606becc
German Ricardo <germanrmo538@gmail.com>
2020-08-08 16:38:23 -0500

    Create perl_rar_descompresions.pl
    Uso de Perl para descompresión de múltiples archivos en Linux (terminal bash Fedora Workstation). 
    Lectura de directorio y contenido Linea 10 a Linea 13. 
    Uso de regex para el filtrado de tipo de archivo --linea 12: push (@list_file, $in_file) if ($in_file =~ /\.rar$/);-- y almacenamiento en @array.

---
+++ perl_rar_descompresions.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use 5.010;
+use warnings;
+use strict;
+
+my @list_file = ();
+my $dir_in;
+my $dir_to_path = '/path/to/dir';
+
+opendir $dir_in, $dir_to_path or die "Cannot open $dir_to_path: $!";
+    foreach my $in_file (readdir $dir_in) {
+                push (@list_file, $in_file) if ($in_file =~ /\.rar$/);
+           }
+      @list_file = sort @list_file;
+            foreach my $i (@list_file) {
+               my $out_file = $dir_to_path.'/'.$i;
+                $out_file =~ /\.rar$/;
+                system 'rar', 'x', $out_file;
+            }
+            closedir $dir_in;
