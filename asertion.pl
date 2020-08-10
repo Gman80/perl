a7b904fcbe7da5269071d6d27dd53a310949a4a0
German Ricardo <germanrmo538@gmail.com>
2020-08-10 17:54:15 -0500

    Add files via upload
    Uso de regex para cambiar de extensión archivos.....
    #perl #regex #bash

---
+++ asertion.pl
@@ -0,0 +1,15 @@
+#!/user/bin/perl
+use warnings;
+use strict;
+#reconocimiento de archivos por extensión y cambio de extensión.
+my @newfiles;
+    foreach my $file (glob "*.*") {
+        push (@newfiles, $file);
+        my $convertfile = $newfiles[-1];
+        $convertfile =~ s/\.old$/.new/;
+        rename $file => $convertfile if ($file =~ /\.old$/);
+        warn "Convert $file to $convertfile..!!!\n" if ($file =~ /\.old$/);
+        warn "Exists $file, no convert!\n" if ($file =~ /\.new$/);
+        warn "file $file no find (.old or .new)" unless ($file =~ /(\.old$|\.new$)/);
+   }
+
